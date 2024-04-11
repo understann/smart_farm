a57651a14ffb3786843f7e6b3f1792ab4c738b91
understann <nattapon.how@gmail.com>
2024-04-12 01:38:15 +0700

    Login Register UI almost finish

---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SvgPicture.asset('assets/smart_farm_logo.svg'),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              'Automatic Planting System',
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.65,
+              child: TextField(
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xffcdcdcd)),
+                        borderRadius: BorderRadius.circular(15)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.green),
+                        borderRadius: BorderRadius.circular(15)),
+                    prefixIcon: Icon(Icons.account_box),
+                    hintText: "Email or username",
+                    labelText: "Email or username"),
+                    
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.65,
+              child: TextField(
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xffcdcdcd)),
+                        borderRadius: BorderRadius.circular(15)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.green),
+                        borderRadius: BorderRadius.circular(15)),
+                    prefixIcon: Icon(Icons.lock),
+                    hintText: "Password",
+                    labelText: "Password"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
