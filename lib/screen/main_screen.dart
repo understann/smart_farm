1d3047cf5369f6594d6ec18f45f21d7600e07523
understann <nattapon.how@gmail.com>
2024-05-09 23:36:30 +0700

    Make Main Screen Scrollable

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -89,123 +89,127 @@ class _MainScreenState extends State<MainScreen> {
               child: CircularProgressIndicator(),
             );
           }
-          return Container(
-            decoration: const BoxDecoration(color: Color(0xFFFAFAFA)),
-            width: double.infinity,
-            height: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(25, 0, 0, 0),
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      height: 120,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(
-                            color: const Color(0xFFCCCCCC), width: 1.0),
-                        borderRadius: BorderRadius.circular(10.0),
+          return SingleChildScrollView(
+            // decoration: const BoxDecoration(color: Color(0xFFFAFAFA)),
+            // width: double.infinity,
+            // height: double.infinity,
+
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      const SizedBox(
+                        height: 20,
                       ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          RichText(
-                            text: const TextSpan(
-                              children: [
-                                TextSpan(
-                                    text: 'Hi! ',
-                                    style: TextStyle(
-                                        fontSize: 40, color: Colors.black)),
-                                TextSpan(
-                                    text: '-',
-                                    style: TextStyle(
-                                        fontSize: 40,
-                                        color: Color(0xFF1B4139))),
-                                TextSpan(
-                                    text: ' Onuma',
-                                    style: TextStyle(
-                                        fontSize: 40, color: Colors.black)),
-                              ],
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(25, 0, 0, 0),
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        height: 120,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(
+                              color: const Color(0xFFCCCCCC), width: 1.0),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RichText(
+                              text: const TextSpan(
+                                children: [
+                                  TextSpan(
+                                      text: 'Hi! ',
+                                      style: TextStyle(
+                                          fontSize: 40, color: Colors.black)),
+                                  TextSpan(
+                                      text: '-',
+                                      style: TextStyle(
+                                          fontSize: 40,
+                                          color: Color(0xFF1B4139))),
+                                  TextSpan(
+                                      text: ' Onuma',
+                                      style: TextStyle(
+                                          fontSize: 40, color: Colors.black)),
+                                ],
+                              ),
+                            ),
+                            const Text(
+                              'How was your planting?',
+                              style: TextStyle(fontSize: 16, color: Colors.black),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      CustomDropdown(
+                        items: documentNames,
+                        value: selectedValue,
+                        onChanged: (newValue) {
+                          setState(
+                            () {
+                              selectedValue = newValue!;
+                            },
+                          );
+                        },
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                       StatusRealtime(plant: selectedValue,),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      const ToggleRealtime(),
+                    ],
+                  ),
+                  SizedBox(height: 30,),
+                  Column(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          launchUrl(
+                            manualURI,
+                            mode: LaunchMode.externalApplication,
+                          );
+                        },
+                        child: const SizedBox(
+                          child: Text(
+                            'user manual',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Color(0xFF656565),
+                              decoration: TextDecoration.underline,
                             ),
                           ),
-                          const Text(
-                            'How was your planting?',
-                            style: TextStyle(fontSize: 16, color: Colors.black),
-                          )
-                        ],
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    CustomDropdown(
-                      items: documentNames,
-                      value: selectedValue,
-                      onChanged: (newValue) {
-                        setState(
-                          () {
-                            selectedValue = newValue!;
-                          },
-                        );
-                      },
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                     StatusRealtime(plant: selectedValue,),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    const ToggleRealtime(),
-                  ],
-                ),
-                Column(
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        launchUrl(
-                          manualURI,
-                          mode: LaunchMode.externalApplication,
-                        );
-                      },
-                      child: const SizedBox(
-                        child: Text(
-                          'user manual',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Color(0xFF656565),
-                            decoration: TextDecoration.underline,
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          context.read<AuthBloc>().add(AuthLogoutRequested());
+                        },
+                        child: const SizedBox(
+                          child: Text(
+                            'Logout',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Color(0xFF656565),
+                              decoration: TextDecoration.underline,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    InkWell(
-                      onTap: () {
-                        context.read<AuthBloc>().add(AuthLogoutRequested());
-                      },
-                      child: const SizedBox(
-                        child: Text(
-                          'Logout',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Color(0xFF656565),
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                )
-              ],
+                    ],
+                  )
+                ],
+              ),
             ),
           );
         }),
